5df7450a0fa83b2e51f874e1a87036189fc3d40c
Lyn <lyn250904@gmail.com>
2026-03-24 11:41:43 +0700

    feat : Cấu hình thư mục pug

---
+++ index.ts
@@ -0,0 +1,30 @@
+import express, { Express, Request, Response } from "express";
+import dotenv from "dotenv"
+import * as database from "./config/database"
+import clientRoutes from "./routes/client/index.route";
+
+const app: Express = express();
+const port: number | string = 3000;
+
+dotenv.config(); // load biến môi trường từ file .env
+
+database.connect(); // kết nối database
+
+
+
+app.set("views", `${__dirname}/views`);
+app.set("view engine", "pug");
+
+app.get("/topics", (req: Request, res: Response) => {
+  res.render("client/pages/topics/index")
+});
+
+
+
+clientRoutes(app);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.listen(port, () => {
+  console.log(`App listening on port ${port}`);
+});
